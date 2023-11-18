e951187ba230b565b312dd7a2846ac9219151110
Berkay Dinç <berkayydinc@gmail.com>
2023-11-18 16:09:56 +0300

    implement send

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-mod error;
-mod message;
+pub mod error;
+pub mod message;
 
 use error::Result;
 
@@ -11,12 +11,13 @@ use tokio::{
     },
 };
 
-use mavlink::{self, MavlinkVersion, Message};
+use mavlink::{self, MavlinkVersion::{ self, V1, V2 }, Message, MavHeader};
 
 pub struct TcpConnection {
     reader: OwnedReadHalf,
     writer: OwnedWriteHalf,
     protocol: MavlinkVersion,
+    sequence: u8,
 }
 
 impl TcpConnection {
@@ -27,15 +28,33 @@ impl TcpConnection {
             reader,
             writer,
             protocol,
+            sequence: 0,
         })
     }
 
     pub async fn receive<M: Message>(&mut self) -> Result<M> {
-        use MavlinkVersion::*;
-
         match self.protocol {
             V1 => message::v1::read(&mut self.reader).await,
             V2 => message::v2::read(&mut self.reader).await,
         }
+    }
+
+    pub async fn send<M: Message>(&mut self, system_id: u8, component_id: u8, message: &M) -> Result<usize> {
+        let header = MavHeader {
+            system_id,
+            component_id,
+            sequence: self.sequence
+        };
+
+        self.sequence += 1;
+
+        match self.protocol {
+            V1 => message::v1::write(&mut self.writer, header, message).await,
+            V2 => message::v2::write(&mut self.writer, header, message).await,
+        }
+    }
+
+    pub fn set_protocol(&mut self, protocol: MavlinkVersion) {
+        self.protocol = protocol;
     }
 }
