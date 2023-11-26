d33f87003309c1e7760f08c1ed964eeb3ba22eab
Berkay Dinç <berkayydinc@gmail.com>
2023-11-26 18:00:04 +0300

    implement connections with traits

--- src/connection.rs
+++ src/connection.rs
@@ -1,58 +1,76 @@
 use crate::error::Result;
 use crate::message;
+use async_trait::async_trait;
 
-use tokio::{
-    self,
-    net::{
-        tcp::{OwnedReadHalf, OwnedWriteHalf},
-        TcpStream, ToSocketAddrs,
-    },
+use mavlink::{ Message, MavHeader };
+use tokio::net::{
+    tcp::{OwnedReadHalf, OwnedWriteHalf},
+    TcpStream, ToSocketAddrs,
 };
 
-use mavlink::{self, MavlinkVersion::{ self, V1, V2 }, Message, MavHeader};
+#[async_trait]
+pub trait Sender {
+    async fn send<M: Message + Sync>(&mut self, system_id: u8, component_id: u8, message: &M) -> Result<usize>;
+}
 
-pub struct TcpConnection {
-    reader: OwnedReadHalf,
+#[async_trait]
+pub trait Receiver: Send {
+    async fn receive<M: Message>(&mut self) -> Result<M>;
+}
+
+#[async_trait]
+pub trait Connection {
+    type Sender: Sender;
+    type Receiver: Receiver;
+    async fn connect<A: ToSocketAddrs + Send>(addr: A) -> Result<(Self::Sender, Self::Receiver)>;
+}
+
+pub struct TcpReceiver(OwnedReadHalf);
+
+#[async_trait]
+impl Receiver for TcpReceiver {
+    async fn receive<M: Message>(&mut self) -> Result<M> {
+        message::v2::read(&mut self.0).await
+    }
+}
+
+pub struct TcpSender {
     writer: OwnedWriteHalf,
-    protocol: MavlinkVersion,
     sequence: u8,
 }
 
-impl TcpConnection {
-    pub async fn connect(addr: impl ToSocketAddrs, protocol: MavlinkVersion) -> Result<Self> {
-        let stream = TcpStream::connect(addr).await?;
-        let (reader, writer) = stream.into_split();
-        Ok(Self {
-            reader,
-            writer,
-            protocol,
-            sequence: 0,
-        })
-    }
-
-    pub async fn receive<M: Message>(&mut self) -> Result<M> {
-        match self.protocol {
-            V1 => message::v1::read(&mut self.reader).await,
-            V2 => message::v2::read(&mut self.reader).await,
-        }
-    }
-
-    pub async fn send<M: Message>(&mut self, system_id: u8, component_id: u8, message: &M) -> Result<usize> {
+#[async_trait]
+impl Sender for TcpSender {
+    async fn send<M>(&mut self, system_id: u8, component_id: u8, message: &M) -> Result<usize>
+        where M: Message + Sync
+    {
         let header = MavHeader {
             system_id,
             component_id,
-            sequence: self.sequence
+            sequence: self.sequence,
         };
 
         self.sequence += 1;
 
-        match self.protocol {
-            V1 => message::v1::write(&mut self.writer, header, message).await,
-            V2 => message::v2::write(&mut self.writer, header, message).await,
-        }
+        message::v2::write(&mut self.writer, header, message).await
     }
+}
 
-    pub fn set_protocol(&mut self, protocol: MavlinkVersion) {
-        self.protocol = protocol;
+pub struct TcpConnection;
+
+#[async_trait]
+impl Connection for TcpConnection {
+    type Sender = TcpSender;
+    type Receiver = TcpReceiver;
+
+    async fn connect<A>(addr: A) -> Result<(Self::Sender, Self::Receiver)>
+        where A: ToSocketAddrs + Send
+    {
+        let stream = TcpStream::connect(addr).await?;
+        let (reader, writer) = stream.into_split();
+        let sender = TcpSender { writer, sequence: 0 };
+        let receiver = TcpReceiver(reader);
+
+        Ok((sender, receiver))
     }
 }
