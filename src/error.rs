11a549bad541fd0e8df3a096a4a160d20221a139
Berkay Dinç <berkayydinc@gmail.com>
2023-12-16 22:18:15 +0300

    implement error and from recverror traits

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,32 @@
-use mavlink;
+use std::io::Error as IoError;
+use tokio::sync::watch::error::RecvError;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-    Io(std::io::Error),
-    MavlinkParserError(mavlink::error::ParserError),
+    Io(IoError),
+    Send,
+    Receive,
+    Timeout,
 }
 
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Self {
+impl std::error::Error for Error { }
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("TODO: impl Display for Error")
+    }
+}
+
+impl From<IoError> for Error {
+    fn from(err: IoError) -> Self {
         Error::Io(err)
     }
 }
 
-impl From<mavlink::error::ParserError> for Error {
-    fn from(err: mavlink::error::ParserError) -> Self {
-        Error::MavlinkParserError(err)
+impl From<RecvError> for Error {
+    fn from(_: RecvError) -> Self {
+        Error::Receive
     }
 }
