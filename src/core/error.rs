9df2731da0bf41031bba383c13a2308452202640
Berkay Dinç <berkayydinc@gmail.com>
2024-01-06 18:44:39 +0300

    remove some of the tokio packages

--- src/core/error.rs
+++ src/core/error.rs
@@ -1,12 +1,10 @@
 use std::io::Error as IoError;
-use tokio::sync::broadcast::error::RecvError;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
     Io(IoError),
-    Recv(RecvError),
     Send,
     Timeout,
 }
@@ -22,11 +20,5 @@ impl std::fmt::Display for Error {
 impl From<IoError> for Error {
     fn from(err: IoError) -> Self {
         Error::Io(err)
-    }
-}
-
-impl From<RecvError> for Error {
-    fn from(err: RecvError) -> Self {
-        Error::Recv(err)
     }
 }
--- src/core/link.rs
+++ src/core/link.rs
@@ -1,22 +1,18 @@
-use crate::{
-    dialect::{Header, Message},
-    wire::Packet,
-};
-
+use crate::{ dialect::{Header, Message}, wire::Packet };
+use async_broadcast::{self as broadcast };
+use flume::{SendError, r#async::SendSink};
 use std::{pin::Pin, task::{Context, Poll}, sync::Arc};
-use tokio::sync::{ mpsc, broadcast };
-use tokio_stream::wrappers::{ReceiverStream, BroadcastStream, errors::BroadcastStreamRecvError};
-use tokio_util::sync::{ PollSender, PollSendError };
 use futures::{Future, future::join, Sink, Stream, StreamExt};
 use pin_project::pin_project;
 
+#[derive(Clone)]
 #[pin_project]
 pub struct Link {
     #[pin]
-    sender: PollSender<(u8, u8, Message)>,
+    sender: SendSink<'static, (u8, u8, Message)>,
 
     #[pin]
-    subscriber: BroadcastStream<Arc<Packet>>,
+    subscriber: broadcast::Receiver<Arc<Packet>>,
 }
 
 impl Link {
@@ -27,12 +23,24 @@ impl Link {
         where T: Sink<Packet>,
               U: Stream<Item = Packet>,
     {
-        let (sender, receiver) = mpsc::channel(64);
-        let (publisher, subscriber) = broadcast::channel(64);
+        let (sender, receiver) = flume::bounded(64);
+        let (mut publisher, subscriber) = broadcast::broadcast(64);
+
+        // Remove the oldest message if the channel is full.
+        publisher.set_overflow(true);
+
+        // Broadcast each incoming message.
+        let shared = Arc::new(publisher);
+        let broadcast = incoming.for_each(move |packet| {
+            let publisher = shared.clone();
+            let packet = Arc::new(packet);
+            async move { let _ = publisher.broadcast(packet).await; }
+        });
 
         // Stamp packets with a sequence number, and forward them to the sink.
         let mut sequence = 0;
-        let forward = ReceiverStream::new(receiver)
+        let forward = receiver
+            .into_stream()
             .map(move |(component_id, system_id, message)| {
                 let header = Header {component_id, system_id, sequence };
                 sequence += 1;
@@ -40,26 +48,15 @@ impl Link {
             })
             .forward(outgoing);
 
-        // Broadcast each incoming message.
-        let shared = Arc::new(publisher);
-        let broadcast = incoming.for_each(move |packet| {
-            let publisher = shared.clone();
-            let packet = Arc::new(packet);
-            async move { let _ = publisher.send(packet); }
-        });
-
         let fut = join(forward, broadcast);
-        let link = Link {
-            sender: PollSender::new(sender),
-            subscriber: BroadcastStream::new(subscriber)
-        };
+        let link = Link { sender: sender.into_sink(), subscriber };
 
         (link, fut)
     }
 }
 
 impl Sink<(u8, u8, Message)> for Link {
-    type Error = PollSendError<(u8, u8, Message)>;
+    type Error = SendError<(u8, u8, Message)>;
 
     fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.project().sender.poll_ready(cx)
@@ -79,7 +76,7 @@ impl Sink<(u8, u8, Message)> for Link {
 }
 
 impl Stream for Link {
-    type Item = Result<Arc<Packet>, BroadcastStreamRecvError>;
+    type Item = Arc<Packet>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         self.project().subscriber.poll_next(cx)
